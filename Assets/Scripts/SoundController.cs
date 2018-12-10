7032ec56689664e8bc1634bd8810da61d0089bf8
ps16atanaka <kyelese0617@gmail.com>
2018-12-10 14:13:53 +0900

    sound周り追加

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : BaseSingletonMono<SoundController>
+{
+    AudioSource audioSource;
+    public AudioSource AudioSource { get { return audioSource; } }
+    AudioClip clip;
+    public AudioClip Clip { set { clip = value; } }
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.loop = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+            audioSource.Play();
+
+        audioSource.clip = clip;
+    }
+}
---
+++ Assets/Scripts/aaa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aaa : MonoBehaviour {
+    [SerializeField]
+    AudioClip clip;
+
+    [SerializeField]
+    AudioClip clip2;
+
+    SoundController a;
+
+	// Use this for initialization
+	void Start () {
+        a = SoundController.Instance;
+        a.AudioSource.clip = clip;
+        //a.Clip = clip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            a.Clip = clip2;
+            Debug.Log("aaaaa");
+        }
+       
+	}
+}
