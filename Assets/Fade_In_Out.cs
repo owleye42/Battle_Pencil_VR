68c85058a2d55d0d5f6c877187c9eafa536352e6
Onuma <31150811+owleye42@users.noreply.github.com>
2019-02-09 20:10:13 +0900

    FadeIO改善

--- Assets/Fade_In_Out.cs
+++ Assets/Fade_In_Out.cs
@@ -28,24 +28,41 @@ public class Fade_In_Out : MonoBehaviour {
         alfaFrag = false;
         a_color = 0;
     }
+    private void Update()
+    {
+      
+        if (Input.GetKey(KeyCode.Space))
+        {
+            FadeIO();
+        }
+    }
 
     public void FadeIO()
     {
         phase = 1;
+        StartCoroutine(StartFade());//暗転
 
-        switch (phase)
+
+    }
+
+    IEnumerator StartFade()
+    {
+        while (once)
         {
-            case 1:
-                StartCoroutine(FadeOut());//暗転
-                break;
-            case 2:
-                StartCoroutine(FadeKeep());//維持
-                break;
-            case 3:
-                StartCoroutine(FadeIn());//明転
-                break;
+            switch (phase)
+            {
+                case 1:
+                    StartCoroutine(FadeOut());//暗転
+                    break;
+                case 2:
+                    StartCoroutine(FadeKeep());//維持
+                    break;
+                case 3:
+                    StartCoroutine(FadeIn());//明転
+                    break;
+            }
+            yield return null;
         }
-
     }
 
     IEnumerator FadeOut()
@@ -60,6 +77,7 @@ public class Fade_In_Out : MonoBehaviour {
                 a_color = 1;
                 alfaFrag = false;
                 phase = 2;
+                Debug.Log("OUT");
                 break;
             }
             yield return null;
@@ -71,11 +89,13 @@ public class Fade_In_Out : MonoBehaviour {
     {
         while (true)
         {
+            Debug.Log("KEEP");
             TimeCount -= 1 * Time.deltaTime;
             if (TimeCount <= 0)
             {
                 TimeCount = 0;
                 phase=3;
+                Debug.Log("KEEP");
                 break;
             }
             yield return null;
@@ -93,6 +113,8 @@ public class Fade_In_Out : MonoBehaviour {
             {
                 a_color = 0;
                 alfaFrag = false;
+                Debug.Log("IN");
+                once = false;
                 break;
             }
             yield return null;
