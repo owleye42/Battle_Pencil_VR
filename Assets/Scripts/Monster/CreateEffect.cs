77c1a3e51b0f1c93308341f14bebf1492817872e
miyake1127 <nasubi20m@gmail.com>
2019-02-20 14:08:57 +0900

    TUIKA

---
+++ Assets/Scripts/Monster/CreateEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEffect : MonoBehaviour {
+    [SerializeField]
+    Vector3 AtackPosition;
+    [SerializeField]
+    float destryTime;
+	
+
+    public void CreateAtackEffect(string effectName)
+    {
+        EffectManager.Instance.CreateEffect(effectName,AtackPosition,destryTime);
+    }
+    
+ }
